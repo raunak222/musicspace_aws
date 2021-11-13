4b15e10e88e2d3a29deeaeeb40a72c41a1b71536
Raunak Sarada <raunaksarda1999@gmail.com>
2021-11-13 12:40:09 +0530

    adding changes

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -10,7 +10,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
     cloudinary,
     params: {
-        folder: 'YelpCamp',
+        folder: 'Bands',
         allowed_formats: ['jpeg', 'png', 'jpg']
     }
 });
